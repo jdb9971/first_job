68dab25fab25c4d2b970a7ca777103c59a3e5234
jdb9971 <79929756+jdb9971@users.noreply.github.com>
2021-03-04 23:03:15 +0530

    version 1.1

--- first_job.py
+++ first_job.py
@@ -1,3 +1,3 @@
-Print("Hello world, this is our first JOB at jenkins")
+print("Hello world, this is our first JOB at jenkins")
 print("We have linked up the Github with jenkins in our first JOB")
 print("Have A great Day :) :) :) ") 
